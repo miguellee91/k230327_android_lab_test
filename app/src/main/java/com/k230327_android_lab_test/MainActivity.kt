60e472d376c82e91c9bfe176aa3aad9cfabb419f
smlee <miguellee91@gmail.com>
2023-07-18 09:21:06 +0900

    first commit

---
+++ app/src/main/java/com/k230327_android_lab_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.k230327_android_lab_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
